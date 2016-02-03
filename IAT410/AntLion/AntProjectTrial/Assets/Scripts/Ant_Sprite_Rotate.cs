49e4800408416c35a943bc894bacae556d638973
Danny Blackstock <dannyblackstock@gmail.com>
2016-02-03 14:14:03 -0800

    Forgot to save the scene before. Now it rotates

---
+++ IAT410/AntLion/AntProjectTrial/Assets/Scripts/Ant_Sprite_Rotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ant_Sprite_Rotate : MonoBehaviour {
+		//Animator anim;
+
+		// Use this for initialization
+		void Start () {
+				//anim = GetComponent<Animator> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+				LookAtMouse ();
+		}
+
+		void LookAtMouse() {
+				var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				Quaternion rot = Quaternion.LookRotation (mousePosition - transform.position, transform.TransformDirection(Vector3.forward));
+				transform.localRotation = new Quaternion(0, 0, rot.z, rot.w);
+		}
+
+}
