315b98a599787c76fc49d08ccc9be37ca60ffb03
Danny Blackstock <dannyblackstock@gmail.com>
2016-02-15 16:43:52 -0800

    Add retry and exit button. Exit button may or may not work 😅

---
+++ IAT410/AntLion/AntProjectTrial/Assets/Scripts/Menus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// function to load levels when buttons pressed
+	public void LoadLevel(string level) {
+		Application.LoadLevel (level);
+	}   
+	// quit game
+	public void QuitApp() {
+		Application.Quit ();
+	}
+}
