976b5a6ca13a3b26b41647e976dc8acca210baa7
Joannalyt <lyt307166987@hotmail.com>
2016-02-16 14:53:41 -0800

    Revert "revert"
    This reverts commit ebd9741db7ca2668af06caff0fa216ef0a9cb5d2.

---
+++ IAT410/AntLion/AntProjectTrial/Assets/Scripts/Bottom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bottom : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision) {
+		
+		if (collision.gameObject.tag == "Bottom") 
+		{
+
+			Destroy(gameObject);
+			
+			
+		}
+	}
+}
