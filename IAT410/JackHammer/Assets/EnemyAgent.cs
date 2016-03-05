e158a8c89dd6113e1e04ca3326d0e4ae18f911e0
Danny Blackstock <dannyblackstock@gmail.com>
2016-03-04 22:30:42 -0800

    enemy chases player

--- IAT410/JackHammer/Assets/EnemyAgent.cs
+++ IAT410/JackHammer/Assets/EnemyAgent.cs
@@ -3,26 +3,32 @@ using System.Collections;
 
 public class EnemyAgent : MonoBehaviour {
     private NavMeshAgent agent;
+    private GameObject player;
+    private Vector3 playerPos;
 
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-                        
-            if (Physics.Raycast(ray, out hit))
-            {
-                if (hit.collider.tag == "Ground")
-                {
-                 agent.SetDestination(hit.point);
-                }
-            }
-        }
+        playerPos = player.transform.position;
+
+          agent.SetDestination(playerPos);
+//        if (Input.GetMouseButtonDown(0))
+//        {
+//            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+//            RaycastHit hit;
+//                        
+//            if (Physics.Raycast(ray, out hit))
+//            {
+//                if (hit.collider.tag == "Ground")
+//                {
+//                 agent.SetDestination(hit.point);
+//                }
+//            }
+//        }
 	}
 }
