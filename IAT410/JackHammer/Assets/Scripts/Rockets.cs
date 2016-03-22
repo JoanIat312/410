8a620a28e5653c45ef3efc110ed56a3cbd0a06dd
Joannalyt <lyt307166987@hotmail.com>
2016-03-21 17:27:40 -0700

    added submarine, but the rocket is not working properly

---
+++ IAT410/JackHammer/Assets/Scripts/Rockets.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rockets : MonoBehaviour {
+
+	public AudioClip explosion;
+	public int moveSpeed;
+	private Vector3 objectPos;
+	private Vector3 targetPos;
+	private Vector3 dis;
+	private Quaternion num;
+	private float angle;
+	private Rigidbody rb;
+	private bool hitWall;
+	public int defaultDamage;
+	Animator anim;
+	public GameManager gameManager;
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+		anim = GetComponent<Animator> ();
+		hitWall = false;
+		defaultDamage = 20;
+		moveSpeed = 10;
+		objectPos = transform.position;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetPos = GameObject.Find ("Player").transform.position;
+		dis = targetPos - objectPos;
+		dis.Normalize ();
+		//rb.velocity = (targetPos.normalized * moveSpeed);
+		transform.Translate (dis+Vector3.left);
+	
+	}
+
+	/*void FixedUpdate ()
+	{
+
+		if (gameObject.name != "Rockets") {
+			if (hitWall == true) {
+				rb.velocity = new Vector3 (0, 0, 0);   
+			} else {
+				rb.velocity = (dis * moveSpeed);
+			}
+		}
+
+	}*/
+}
---
+++ IAT410/JackHammer/Assets/Scripts/SubmarineAgent.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubmarineAgent : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	public GameManager gameManager;
+	private GameObject player;
+	public State state;
+	private bool alive;
+	public GameObject sprite;
+	private Vector3 dis;
+	private RaycastHit hit;
+	public float firingRange = 2.5f;
+	public float sightDist = 10;
+	private float nextBulletSpawnTimestamp;
+	public float defaultFireRate = .8f;
+	public GameObject bObject;
+	public AudioClip shot;
+
+	public enum State
+	{
+		HIDE,
+		POP,
+		ATTACK
+	}
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+		player = GameObject.Find ("Player");
+		agent.updatePosition = true;
+		agent.updateRotation = false;
+		alive = true;
+		state = SubmarineAgent.State.ATTACK;
+		StartCoroutine ("FSM");
+	}
+	
+
+	IEnumerator FSM ()
+	{
+		while (alive) {
+			switch (state) {
+			case State.HIDE:
+				Hide ();
+				break;
+			case State.POP:
+				Pop ();
+				break;
+			case State.ATTACK:
+				Attack ();
+				break;
+			}
+
+			yield return null;
+		}
+	}
+
+	void Hide(){
+		agent.speed = 0;
+		sprite.SetActive (false);
+
+	}
+
+	void Pop(){
+		sprite.SetActive (true);
+	}
+
+	void Attack(){
+		if (Time.time >= nextBulletSpawnTimestamp && GameManager.stunEnemies == false) {
+			nextBulletSpawnTimestamp = Time.time + defaultFireRate;
+			GameObject newBullet = Instantiate (bObject, sprite.transform.position, sprite.transform.rotation) as GameObject;
+			AudioSource.PlayClipAtPoint (shot, transform.position);
+			newBullet.tag = "bullets";
+		}
+	}
+
+	void Update () {
+		dis = transform.position - player.transform.position;
+		Debug.DrawRay (transform.position, -dis, Color.green);
+		if (Physics.Raycast (transform.position, -dis, out hit, sightDist)) {
+			Debug.Log (hit.collider.gameObject.tag);
+			if (hit.collider.gameObject.tag == "Player") {
+				//state = SubmarineAgent.State.POP;
+				if ((dis.z < firingRange && dis.z > -firingRange) && (dis.x < firingRange && dis.x > -firingRange)) {
+					state = SubmarineAgent.State.ATTACK;
+				}
+			} else {
+				//state = SubmarineAgent.State.HIDE;
+			}
+		}
+	}
+}
