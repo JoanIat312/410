a4b08617e253281b83e94744d4f696058e03993f
Danny Blackstock <dannyblackstock@gmail.com>
2016-04-02 15:08:07 -0700

    start doing cutscene movie playing. add tutorial map

---
+++ IAT410/JackHammer/Assets/MoviePlay.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlay : MonoBehaviour {
+    
+    public MovieTexture movTexture;
+
+    // Use this for initialization
+    void Start() {
+//        GetComponent<Renderer>().material.mainTexture = movTexture;
+//        movTexture.Play();
+
+            Renderer r = GetComponent<Renderer>();
+            MovieTexture movie = (MovieTexture)r.material.mainTexture;
+
+//            if (movie.isPlaying) {
+//                movie.Pause();
+//            }
+//            else {
+                movie.Play();
+//            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
